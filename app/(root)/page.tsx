7b4367047841fb238a2192e07ec605d43d34ffac
mstout60 <mstout@eprospector.com>
2025-01-14 17:04:51 -0600

    Setup Pulic images and icons and create root group with layout/page

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import BookList from "@/components/book-list";
+import BookOverview from "@/components/book-overview";
+import { Button } from "@/components/ui/button";
+
+const Home = () => (
+  <>
+   <BookOverview />
+   <BookList />
+  </>
+);
+
+export default Home;
