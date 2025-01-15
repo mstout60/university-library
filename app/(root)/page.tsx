6c4640beef4e4728eefc86188b460b9fdb12c4f3
mstout60 <mstout@eprospector.com>
2025-01-15 12:27:31 -0600

    Implement Home page with book cover and book list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,15 @@
 import BookList from "@/components/book-list";
 import BookOverview from "@/components/book-overview";
-import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
   <>
-   <BookOverview />
-   <BookList />
+    <BookOverview {...sampleBooks[0]} />
+    <BookList
+      title="Latest Books"
+      books={sampleBooks}
+      containerClassName="mt-28"
+    />
   </>
 );
 
