a4b7f2ac830859073c0ea88ca1eea66165b11507
mstout60 <mstout@eprospector.com>
2025-01-20 19:16:26 -0600

    Fixed issue with onboard route

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -1,6 +1,6 @@
+import { serve } from "@upstash/workflow/nextjs";
 import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
-import { serve } from "@upstash/workflow/nextjs";
 import { eq } from "drizzle-orm";
 import { sendEmail } from "@/lib/workflow";
 
@@ -13,7 +13,7 @@ type InitialData = {
 
 const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
 const THREE_DAYS_IN_MS = 3 * ONE_DAY_IN_MS;
-const THRITY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
+const THIRTY_DAYS_IN_MS = 30 * ONE_DAY_IN_MS;
 
 const getUserState = async (email: string): Promise<UserState> => {
   const user = await db
@@ -29,8 +29,8 @@ const getUserState = async (email: string): Promise<UserState> => {
   const timeDifference = now.getTime() - lastActivityDate.getTime();
 
   if (
-    timeDifference > THRITY_DAYS_IN_MS &&
-    timeDifference <= THRITY_DAYS_IN_MS
+    timeDifference > THREE_DAYS_IN_MS &&
+    timeDifference <= THIRTY_DAYS_IN_MS
   ) {
     return "non-active";
   }
@@ -41,16 +41,16 @@ const getUserState = async (email: string): Promise<UserState> => {
 export const { POST } = serve<InitialData>(async (context) => {
   const { email, fullName } = context.requestPayload;
 
-  //Welcome email
+  // Welcome Email
   await context.run("new-signup", async () => {
     await sendEmail({
       email,
       subject: "Welcome to the platform",
-      message: `Welcome ${fullName}`,
+      message: `Welcome ${fullName}!`,
     });
   });
 
-  await context.sleep("wait-for-3-days", THREE_DAYS_IN_MS);
+  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
 
   while (true) {
     const state = await context.run("check-user-state", async () => {
@@ -62,7 +62,7 @@ export const { POST } = serve<InitialData>(async (context) => {
         await sendEmail({
           email,
           subject: "Are you still there?",
-          message: `Hey ${fullName}, we miss you`,
+          message: `Hey ${fullName}, we miss you!`,
         });
       });
     } else if (state === "active") {
@@ -70,11 +70,11 @@ export const { POST } = serve<InitialData>(async (context) => {
         await sendEmail({
           email,
           subject: "Welcome back!",
-          message: `Welcome back ${fullName}`,
+          message: `Welcome back ${fullName}!`,
         });
       });
     }
 
-    await context.sleep("wait-for-1-month", THRITY_DAYS_IN_MS);
+    await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
   }
 });
