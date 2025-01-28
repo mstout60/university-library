0765114f9260b3188717ea68ff21df217ff1563c
mstout60 <mstout@eprospector.com>
2025-01-28 00:49:24 -0600

    Implement Borrow Book with update to my-profile

--- app/(root)/my-profile/page.tsx
+++ app/(root)/my-profile/page.tsx
@@ -1,24 +1,37 @@
-import { signOut } from "@/auth";
+import { auth } from "@/auth";
 import BookList from "@/components/book-list";
-import { Button } from "@/components/ui/button";
-import { sampleBooks } from "@/constants";
+
+import { db } from "@/database/drizzle";
+import { books, borrowRecords } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 import React from "react";
 
-const page = () => {
+const page = async () => {
+  const session = await auth();
+  const borrowedList = await db
+    .select({
+      id: books.id,
+      title: books.title,
+      author: books.author,
+      genre: books.genre,
+      rating: books.rating,
+      coverUrl: books.coverUrl,
+      coverColor: books.coverColor,
+      description: books.description,
+      totalCopies: books.totalCopies,
+      availableCopies: books.availableCopies,
+      videoUrl: books.videoUrl,
+      summary: books.summary,
+      createdAt: books.createdAt,
+    })
+    .from(borrowRecords)
+    .where(eq(borrowRecords.userId, session?.user?.id as string))
+    .innerJoin(books, eq(books.id, borrowRecords.bookId));
+
   return (
     <>
-      <form
-        action={async () => {
-          "use server";
-
-          await signOut();
-        }}
-        className="mb-10"
-      >
-        <Button>Logout</Button>
-      </form>
-      <BookList title="Borrowed books" books={sampleBooks} />
+      <BookList title="Borrowed books" books={borrowedList} />
     </>
   );
 };
--- components/book-overview.tsx
+++ components/book-overview.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import React from "react";
-import { Button } from "@/components/ui/button";
 import BookCover from "@/components/book-cover";
+import BorrowBook from "@/components/borrow-book";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 
 interface Props extends Book {
   userId: string;
 }
 
-const BookOverview = ({
+const BookOverview = async ({
   title,
   author,
   genre,
@@ -20,6 +23,20 @@ const BookOverview = ({
   id,
   userId,
 }: Props) => {
+  const [user] = await db
+    .select()
+    .from(users)
+    .where(eq(users.id, userId))
+    .limit(1);
+
+  const borrowingEligibility = {
+    isEligible: availableCopies > 0 && user.status === "APPROVED",
+    message:
+      availableCopies <= 0
+        ? "Book is not available"
+        : "You are not eligible to borrow this book",
+  };
+
   return (
     <section className="book-overview">
       <div className="flex flex-1 flex-col gap-5">
@@ -46,10 +63,13 @@ const BookOverview = ({
           </p>
         </div>
         <p className="book-description">{description}</p>
-        <Button className="book-overview_btn">
-          <Image src="/icons/book.svg" alt="book" width={20} height={20} />
-          <p className="font-bebas-neue text-xl text-dark-100">Borrow</p>
-        </Button>
+        {user && (
+          <BorrowBook
+            bookId={id}
+            userId={userId}
+            borrowingEligibility={borrowingEligibility}
+          />
+        )}
       </div>
       <div className="relative flex flex-1 justify-center">
         <div className="relative">
